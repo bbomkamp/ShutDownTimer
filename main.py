e85dad9349a9e77f7136e843bdf57d3c7eb19843
bbomkamp <bradbomkamp@gmail.com>
2023-01-31 17:27:17 -0600

    Added functional cancel button

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import tkinter as tk
-import time
 import os
 import tkinter.ttk as ttk
 
 def countdown(remaining_time, total_time):
+    global after_id
     remaining_time -= 1
 
     # Calculate the remaining hours and seconds
@@ -17,18 +17,18 @@ def countdown(remaining_time, total_time):
 
     # Update the progress circle
     canvas.delete("all")
-    canvas.create_arc(10, 10, 190, 190, start=0, extent=360-(remaining_time/total_time)*360, fill="#16c5f5", width=5)
+    canvas.create_arc(10, 10, 190, 190, start=0, extent=360-(remaining_time/total_time)*360, fill="#16c5f5", width=2)
 
     if remaining_time <= 0:
         os.system("shutdown /s /t 1")
     else:
         # Schedule the next iteration of the countdown
-        root.after(1000, countdown, remaining_time, total_time)
+        after_id = root.after(1000, countdown, remaining_time, total_time)
 
 # Initialize app (Initialize Window Object)
 root = tk.Tk()
 root.title("Shut Down Timer")
-root.geometry("600x400")
+root.geometry("600x600")
 root.eval("tk::PlaceWindow . center")  # Centers Window at Launch.
 
 frame1 = tk.Frame(root, bg="#3d6466")
@@ -45,15 +45,17 @@ time_type_combo.current(0)
 time_type_combo.pack(pady=10)
 
 # Add a label to display the remaining time
-remaining_time_label = tk.Label(frame1, text="", font=("TkDefaultFont", 14))
+remaining_time_label = tk.Label(frame1, text="", font=("TkDefaultFont", 14), bg="#3d6466", fg="white")
 remaining_time_label.pack(pady=10)
 
 # Add a canvas for the progress circle
-canvas = tk.Canvas(frame1, width=200, height=200, bg="#3d6466")
+canvas = tk.Canvas(frame1, width=200, height=200, bg="#3d6466", highlightthickness=0)
 canvas.pack(pady=10)
 
+after_id = None
 
 def start_countdown():
+    global after_id
     time = int(time_entry.get())
     time_type = time_type_combo.get()
 
@@ -66,10 +68,24 @@ def start_countdown():
         os.system("shutdown /s /t 1")
     else:
         # Schedule the first iteration of the countdown
-        root.after(1000, countdown, time, time)
+        after_id = root.after(1000, countdown, time, time)
+
 
 # Add a button to start the countdown
 start_button = tk.Button(frame1, text="Start", command=start_countdown)
 start_button.pack(pady=10)
+
+def stop_countdown():
+    global after_id
+    if after_id:
+        root.after_cancel(after_id)
+        after_id = None
+        remaining_time_label.config(text="")
+        canvas.delete("all")
+
+# Add a button to cancel the countdown
+cancel_button = tk.Button(frame1, text="Cancel", command=stop_countdown)
+cancel_button.pack(pady=20)
+
 
 root.mainloop()
