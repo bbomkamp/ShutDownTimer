28bf299369fe2bf07c60b3e27a9889db85d6b30c
bbomkamp <bradbomkamp@gmail.com>
2023-01-31 14:55:29 -0600

    Window sized with user input

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
 import tkinter as tk
+import time
+import os
 
-# Initiallize app (Init Window Object)
+# Initialize app (Initialize Window Object)
 root = tk.Tk()
 root.title("Shut Down Timer")
-root.eval("tk::PlaceWindow . center") # Centers Window at Launch.
+root.geometry("600x400")
+root.eval("tk::PlaceWindow . center")  # Centers Window at Launch.
 
-frame1 = tk.Frame(root, width=600, height=400, bg="#3d6466")
-frame1.grid(row=0, column=0)
+frame1 = tk.Frame(root, bg="#3d6466")
+frame1.pack(fill="both", expand=True)
+
+# Add Entry widget for user input
+time_entry = tk.Entry(frame1)
+time_entry.pack(pady=10)
+
+# Add a button to start the countdown
+start_button = tk.Button(frame1, text="Start", command=root.quit)
+start_button.pack(pady=10)
+
+def countdown():
+    time = int(time_entry.get())
+
+    if time <= 0:
+        os.system("shutdown /s /t 1")
+    else:
+        # Update the UI with the new time
+        time_entry.config(state="normal")
+        time_entry.delete(0, "end")
+        time_entry.insert(0, time)
+        time_entry.config(state="readonly")
+
+        # Schedule the next iteration of the countdown
+        root.after(1000, countdown, time - 1)
 
 # Run app (Display Window)
 root.mainloop()
+
+# Start the countdown after the user has entered the time
+countdown()
